56a206aa5c77c8e9cb4c35e38a3e5737051a367d
Pete Bunting <petebunting@mac.com>
2019-11-10 15:07:51 +0000

    Update

--- app/routes.py
+++ app/routes.py
@@ -13,9 +13,13 @@ from wtforms import SubmitField, SelectField, DateField
 from wtforms.validators import Required
 #from wtforms.fields.html5 import DateField
 
+import eodatadown.eodatadownsystemmain
+
+CONFIG_FILE = "/Users/pete/Temp/wales_ard_monitoring/eodd/config/EODataDownBaseConfig_psql.json"
+
 class SelectDataForm(FlaskForm):
-    start_date = DateField('Start', validators=[Required()], format='%d/%m/%Y', default=datetime.date(1980, 1, 1), description='Start Date')
-    end_date = DateField('End', validators=[Required()], format='%d/%m/%Y', default=datetime.date.today, description='End Date')
+    start_date = DateField('Start', validators=[Required()], format='%Y/%m/%d', default=datetime.date(1980, 1, 1), description='Start Date')
+    end_date = DateField('End', validators=[Required()], format='%Y/%m/%d', default=datetime.date.today, description='End Date')
     sensor_field = SelectField('Sensor', choices=[('Landsat', 'Landsat'), ('Sentinel-1', 'Sentinel-1'), ('Sentinel-2', 'Sentinel-2')] , validators=[Required()], default='Landsat')
     go_submit = SubmitField('Go')
 
@@ -30,10 +34,41 @@ def index():
 
 @app.route('/imagelist', methods=['POST'])
 def imagelist():
-    print("HERE - 3")
+    try:
+        sys_main_obj = eodatadown.eodatadownsystemmain.EODataDownSystemMain()
+        sys_main_obj.parse_config(CONFIG_FILE)
+        sensor_sys_objs = sys_main_obj.get_sensors()
+    except Exception:
+        flash('Something has gone wrong either the database was found or there is an error with the configuration file.')
+        return redirect('index.html')
+    
     form = request.form
     start_date = form['start_date']
     end_date = form['end_date']
-    sensor = form['sensor_field']
+    sensor_str = form['sensor_field']
+    
+    print(start_date)
+    print(end_date)
+    print(sensor)
     print("HERE - 5")
+    
+    sensor_obj = None
+    found_sensor_obj = False
+    for sensor_sys_obj in sensor_sys_objs:
+        if (sensor_str == 'Landsat') and (sensor_sys_obj.get_sensor_name() == "LandsatGOOG"):
+            sensor_obj = sensor_sys_obj
+            found_sensor_obj = True
+        elif (sensor_str == 'Sentinel-1') and (sensor_sys_obj.get_sensor_name() == "Sentinel1ASF"):
+            sensor_obj = sensor_sys_obj
+            found_sensor_obj = True
+        elif (sensor_str == 'Sentinel-2') and (sensor_sys_obj.get_sensor_name() == "Sentinel2GOOG"):
+            sensor_obj = sensor_sys_obj
+            found_sensor_obj = True
+
+    if found_sensor_obj:
+        print("HERE - 6")
+    else:
+        flash('Something has gone wrong could not find the sensor specfied.')
+        return redirect('index.html')
+    
     return render_template('imagelist.html')
