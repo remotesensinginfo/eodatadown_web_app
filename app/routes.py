7c9a4ebda21304e6694fbd73913c5193ad4ad8bd
Pete Bunting <petebunting@mac.com>
2019-11-16 15:18:18 +0000

    Updated starting to retrieve info for displaying image grid.

--- app/routes.py
+++ app/routes.py
@@ -5,6 +5,7 @@ import json
 import uuid
 import time
 import datetime
+import math
 
 from app import app
 
@@ -15,6 +16,8 @@ from wtforms.validators import Required
 import eodatadown.eodatadownsystemmain
 
 CONFIG_FILE = "/bigdata/eodd_wales_ard/scripts/eodd/config/EODataDownBaseConfig_psql.json"
+
+N_SCNS_PAGE = 16
 
 class SelectDataForm(FlaskForm):
     start_date = DateField('Start', validators=[Required()], format='%Y/%m/%d', default=datetime.date(1980, 1, 1), description='Start Date')
@@ -31,7 +34,7 @@ def index():
     return render_template('index.html', form=form)
 
 
-@app.route('/imagelist', methods=['POST'])
+@app.route('/imagelist', methods=['GET', 'POST'])
 def imagelist():
     try:
         sys_main_obj = eodatadown.eodatadownsystemmain.EODataDownSystemMain()
@@ -46,6 +49,9 @@ def imagelist():
     start_date = form['start_date']
     end_date = form['end_date']
     sensor_str = form['sensor_field']
+
+    start_date_obj = datetime.datetime.strptime(start_date, '%Y\%m%d')
+    end_date_obj = datetime.datetime.strptime(end_date, '%Y\%m%d')
     
     print(start_date)
     print(end_date)
@@ -65,11 +71,45 @@ def imagelist():
             sensor_obj = sensor_sys_obj
             found_sensor_obj = True
 
+    imgs_dict = dict()
     if found_sensor_obj:
         print("HERE - 6")
+        n_scns = sensor_obj.query_scn_records_date_count(start_date_obj, end_date_obj, valid=True)
+        print("N Scenes: {}".format(n_scns))
+
+        n_full_pages = math.floor(n_scns / N_SCNS_PAGE)
+        remain_scns = n_scns - (n_full_pages * N_SCNS_PAGE)
+        print("{} Full Pages with {} remaining".format(n_full_pages, remain_scns))
+        start_rec = 0
+        if n_full_pages > 0:
+            scns = sensor_obj.query_scn_records_date(start_date, end_date, start_rec, N_SCNS_PAGE, valid=True)
+        else:
+            scns = sensor_obj.query_scn_records_date(start_date, end_date, start_rec, remain_scns, valid=True)
+
+
+        for scn in scns:
+            imgs_dict[scn.PID] = dict()
+            if sensor_str == 'Landsat':
+                imgs_dict[scn.PID]['id'] = scn.Scene_ID
+                imgs_dict[scn.PID]['date'] = scn.Date_Acquired.strftime('%Y-%m-%d')
+            elif sensor_str == 'Sentinel-1':
+                imgs_dict[scn.PID]['id'] = scn.Scene_ID
+                imgs_dict[scn.PID]['date'] = scn.Acquisition_Date.strftime('%Y-%m-%d')
+            elif sensor_str == 'Sentinel-2':
+                imgs_dict[scn.PID]['id'] = scn.Granule_ID
+                imgs_dict[scn.PID]['date'] = scn.Sensing_Time.strftime('%Y-%m-%d')
+            else:
+                raise Exception("Do not recognise the sensor provided.")
+            qk_imgs = scn.ExtendedInfo["quicklook"]["quicklookimgs"]
+            qk_sm_img = ''
+            for qk_img in qk_imgs:
+                if '250px' in qk_img:
+                    qk_sm_img = qk_img
+                    break
+            imgs_dict[scn.PID]['img'] = qk_sm_img
     else:
         print("Error - didn't not find the sensor object.")
         flash('Something has gone wrong could not find the sensor specfied.')
         return redirect('/')
     
-    return render_template('imagelist.html')
+    return render_template('imagelist.html', scns=imgs_dict, sensor=sensor_str )
